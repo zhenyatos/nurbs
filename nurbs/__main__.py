7c7f9492329bf6f9a581b2f297920ce51dbddfc5
via8 <viaborne8@gmail.com>
2021-03-23 11:01:58 +0300

    Update UI (2)

--- nurbs/__main__.py
+++ nurbs/__main__.py
@@ -39,20 +39,36 @@ def main():
 		__canvas.get_size()[0] - const.WORKSPACE_SIZE_X,
 		__canvas.get_size()[1] - const.WORKSPACE_SIZE_Y))
 	canvas_font = pg.font.SysFont(const.CANVAS_FONT['style'], const.CANVAS_FONT['size'])
+	canvas_font_w, canvas_font_h = canvas_font.size('?')
+	canvas_size = __canvas.get_size()
+
+	hints = [
+		'     lbutton click: add point',
+		'     lbutton hold: drag point',
+		'  rbutton click: remove point',
+		'wheel up/down: inc/dec degree',
+	]
+
+	hints_size = (max([len(hint) for hint in hints]) * canvas_font_w, len(hints) * canvas_font_h)
+	hints_surf = pg.Surface(hints_size).convert()
+	hints_surf.fill(const.COLOR_CANVAS)
+	for i in range(len(hints)):
+		hints_surf.blit(canvas_font.render(hints[i], True, const.CANVAS_FONT['color']), (0, canvas_font_h * i))
 
 	run = True
 	mouse_button_up = 0
 	mouse_button_down = 0
 	while run:
 		__canvas.fill(const.COLOR_CANVAS)
-		__canvas.blit(canvas_font.render('lbutton click: add point', True, const.CANVAS_FONT['color']),
-			(const.CANVAS_FONT['indent'], 0))
-		__canvas.blit(canvas_font.render('lbutton hold: drag point', True, const.CANVAS_FONT['color']),
-			(const.CANVAS_FONT['indent'], const.CANVAS_FONT['size']))
-		__canvas.blit(canvas_font.render('rbutton click: remove point', True, const.CANVAS_FONT['color']),
-			(const.CANVAS_FONT['indent'], const.CANVAS_FONT['size'] * 2))
-		__canvas.blit(canvas_font.render('wheel up/down: inc/dec degree', True, const.CANVAS_FONT['color']),
-			(const.CANVAS_FONT['indent'], const.CANVAS_FONT['size'] * 3))
+		__canvas.blit(hints_surf, (canvas_size[0] - hints_size[0] - const.CANVAS_FONT['indent'], const.CANVAS_FONT['indent']))
+		# __canvas.blit(canvas_font.render('lbutton click: add point', True, const.CANVAS_FONT['color']),
+		# 	(const.CANVAS_FONT['indent'], 0))
+		# __canvas.blit(canvas_font.render('lbutton hold: drag point', True, const.CANVAS_FONT['color']),
+		# 	(const.CANVAS_FONT['indent'], const.CANVAS_FONT['size']))
+		# __canvas.blit(canvas_font.render('rbutton click: remove point', True, const.CANVAS_FONT['color']),
+		# 	(const.CANVAS_FONT['indent'], const.CANVAS_FONT['size'] * 2))
+		# __canvas.blit(canvas_font.render('wheel up/down: inc/dec degree', True, const.CANVAS_FONT['color']),
+		# 	(const.CANVAS_FONT['indent'], const.CANVAS_FONT['size'] * 3))
 
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
